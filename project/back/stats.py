6086aa1019231a4e8c605e77aff3eeda2d2335fe
clement.gregoire <clement.gregoire@epita.fr>
2026-02-18 20:08:12 +0100

    ajout stats.py + 2 graphiques

---
+++ project/back/stats.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
+
+def get_pandas():
+    # get the pandas dataframe
+    NotImplementedError()
+    # return df
+
+
+df = pd.DataFrame(
+    {
+        "size": [(100, 100), (200, 200), (300, 300)],
+        "time": [7, 8, 9],
+        "time_opti": [10, 11, 12],
+    },
+    index=["img1", "img2", "img3"],
+)
+
+df["size_val"] = df["size"].apply(lambda x: x[0] * x[1])
+
+
+def time_vs_size(df):
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(x=df["size_val"], y=df["time"], mode="lines+markers", name="time")
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=df["size_val"], y=df["time_opti"], mode="lines+markers", name="time_opti"
+        )
+    )
+    fig.update_layout(
+        xaxis_title="Size (number of pixel)",
+        yaxis_title="Time",
+        title="Time vs Size",
+        legend_title="Courbes",
+    )
+    return fig
+
+
+def bar_chart_time(df):
+    last_row = df.tail(1)
+    fig = go.Figure(
+        [go.Bar(x=["pas opti", "opti"], y=last_row[["time", "time_opti"]].values.flatten())]
+    )
+    return fig
+
+def main():
+    fig = make_subplots(rows=1, cols=2)
+    fig1 = time_vs_size(df)
+    fig2 = bar_chart_time(df)
+    for trace in fig1.data:
+        fig.add_trace(trace, row=1, col=1)
+    for trace in fig2.data:
+        fig.add_trace(trace, row=1, col=2)
+    
+    fig.show()
+
+
+main()
